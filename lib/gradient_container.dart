8629cf7a91c9dfa26f1219a0ba0423bf0af6a4ff
Dmitry Ilyin <mowgle>
2023-05-08 13:29:01 +0300

    feat: add buttons and use functions as values

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,6 +9,8 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> colors;
 
+  void rollDice() {}
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,9 +22,35 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset(
-          'assets/images/dice-1.png',
-          width: 250,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 250,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // ElevatedButton(
+            //   onPressed: rollDice,
+            //   child: const Text('Roll Dice'),
+            // ),
+            // OutlinedButton(
+            //   onPressed: rollDice,
+            //   child: const Text('Roll Dice'),
+            // ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                  // padding: const EdgeInsets.only(top: 20),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 28,
+                  )),
+              child: const Text('Roll Dice'),
+            )
+          ],
         ),
       ),
     );
